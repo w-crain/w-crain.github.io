5d1815f57836ef97b75bac882d9ed7b41b89e25e
w-crain <81821679+w-crain@users.noreply.github.com>
2023-08-08 06:45:35 -0500

    Adding Dark/Light mode themes and toggles

---
+++ script.js
@@ -0,0 +1,21 @@
+const toggleButton = document.getElementById("themeToggle");
+
+var storedTheme =
+  localStorage.getItem("theme") ||
+  (window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light");
+if (storedTheme)
+  document.documentElement.setAttribute("data-theme", storedTheme);
+
+toggleButton.onclick = function () {
+  var currentTheme = document.documentElement.getAttribute("data-theme");
+  var targetTheme = "light";
+
+  if (currentTheme === "light") {
+    targetTheme = "dark";
+  }
+
+  document.documentElement.setAttribute("data-theme", targetTheme);
+  localStorage.setItem("theme", targetTheme);
+};
